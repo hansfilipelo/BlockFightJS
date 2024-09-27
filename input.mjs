f240277e409d94a2564c520e68610723ab02a7ee
Hans-Filip Elo <hansfilipelo@gmail.com>
2024-09-27 11:29:02 +0200

    Add input interceptor
    This patch adds an input interceptor to not have to rewrite important
    game input features in multiple input handlers.

--- input.mjs
+++ input.mjs
@@ -1,13 +1,81 @@
 
+class InputInterceptor {
+  constructor(player) {
+    this.player_ = player;
+    this.down_pressed_ = false;
+    this.up_pressed_ = false;
+    this.left_pressed_ = false;
+    this.right_pressed_ = false;
+    this.space_pressed_ = false;
+  }
+  onDownPressed() {
+    if (!this.down_pressed_) {
+      this.down_pressed_ = true;
+      this.player_.drop();
+    }
+  }
+
+  onDownReleased() {
+    this.down_pressed_ = false;
+  }
+
+  onUpPressed() {
+    if (!this.up_pressed_) {
+      this.up_pressed_ = true;
+      this.player_.rotate();
+    }
+  }
+
+  onUpReleased() {
+    this.up_pressed_ = false;
+  }
+
+  onLeftPressed() {
+    if (!this.left_pressed_) {
+      this.left_pressed_ = true;
+        this.player_.left();
+    }
+  }
+
+  onLeftReleased() {
+    this.left_pressed_ = false;
+  }
+
+  onRightPressed() {
+    if (!this.right_pressed_) {
+      this.right_pressed_ = true;
+      this.player_.right();
+    }
+  }
+
+  onRightReleased() {
+    this.right_pressed_ = false;
+  }
+
+  onSpacePressed() {
+    if (!this.space_pressed_) {
+        this.space_pressed_ = true;
+      this.player_.dropAllTheWay();
+    }
+  }
+
+  onSpaceReleased() {
+    this.space_pressed_ = false;
+  }
+}
+
 export async function createInputHandler(player, platform=null) {
+  let input_interceptor = new InputInterceptor(player);
+
   if (!platform) {
     return await import("./input/kaplay.mjs").then(
-      module => module.default(player));
+      module => module.default(input_interceptor));
   }
+
 
   if (platform === "kaplay") {
     return await import("./input/kaplay.mjs").then(
-      module => module.default(player));
+      module => module.default(input_interceptor));
   }
 
   // TODO: Implement other input methods.
