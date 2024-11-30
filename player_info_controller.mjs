f5b8dd8c682dc625acd2fdb78b6c6a20f033aebc
Hans-Filip Elo <hansfilipelo@gmail.com>
2024-11-30 18:17:11 +0100

    First-implementation of drop-timer and start-game features

---
+++ player_info_controller.mjs
@@ -0,0 +1,35 @@
+
+
+function startGameCallback(player_info_controller) {
+  player_info_controller.startGame();
+}
+
+class PlayerInfoController {
+
+  constructor(menu_div) {
+    this.menu_div_ = menu_div;
+    this.start_button_ = this.menu_div_.querySelector("#start_button_button");
+    this.start_button_.addEventListener("click", () => {startGameCallback(this)});
+    this.start_game_callback_ = null;
+  }
+
+  setStartGameCallback(start_game_callback) {
+    this.start_game_callback_ = start_game_callback;
+  }
+
+  getPlayerName() {
+    return this.menu_div_.querySelector("#player_name_input").value;
+  }
+
+  setScore(score) {
+    this.menu_div_.querySelector("#score_value").textContent = score;
+  }
+
+  startGame() {
+    console.assert(this.start_game_callback_ != null);
+    this.start_game_callback_(this.getPlayerName());
+  }
+
+};
+
+export { PlayerInfoController };
