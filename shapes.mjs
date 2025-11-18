bdf5bc982deabba4179ed3329a51b341ed3b8627
Hans-Filip Elo <hansfilipelo@gmail.com>
2025-11-18 20:38:57 +0100

    Add O-shape

--- shapes.mjs
+++ shapes.mjs
@@ -1,9 +1,10 @@
 import { IShape } from "./i_shape.mjs"
 import { SShape } from "./s_shape.mjs"
 import { ZShape } from "./z_shape.mjs"
+import { OShape } from "./o_shape.mjs"
 
 function randomShape() {
-  const shapes = [IShape, SShape, ZShape];
+  const shapes = [IShape, SShape, ZShape, OShape];
   const idx = Math.floor(Math.random() * shapes.length);
   return shapes[idx];
 }
